9b9ffe55624ebb816be499fdc43734318aea4257
bmsohwinc <bharasou58@gmail.com>
2022-03-13 14:24:16 +0530

    done with the initial version

--- README.md
+++ README.md
@@ -3,7 +3,12 @@ Following - https://www.youtube.com/watch?v=IO1tjm9gcIs
 
 ## Install
 - Basic: `create-react-app`
-- Tensorflow related: @tensorflow/tfjs, @tensorflow-models/qna
+- Tensorflow related: `@tensorflow/tfjs, @tensorflow-models/qna`
 - For UI: `react-loader-spinner`
 
-## 
+## Steps
+- Install dependencies
+
+## Note
+- Couldn't implement the code on local due to npm version issues
+- check the code on codesandbo.io: https://codesandbox.io/embed/laughing-microservice-rqzjse?fontsize=14&hidenavigation=1&theme=dark
--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Fragment } from "react";
+import * as tf from "@tensorflow/tfjs";
+import * as qna from "@tensorflow-models/qna";
+// import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
+import Loader from "react-loader-spinner";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <header>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
